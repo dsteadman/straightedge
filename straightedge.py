33d666994e9fbece139a38914f7ffe5499a2ec02
Dan Steadman <steadman.dan@gmail.com>
2016-09-22 09:06:26 -0400

    test newspaper content

--- straightedge.py
+++ straightedge.py
@@ -1,3 +1,4 @@
+import newspaper
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,4 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    cnn_paper = newspaper.build('http://cnn.com', memoize_articles=False)
+    articles = cnn_paper.articles
+    return render_template('homepage.html', articles=articles)
